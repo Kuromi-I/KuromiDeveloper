c2ceca6630ce571e9e7a648aceec8ff765ef1aa3
Isa Vieira <isadoravieira2001@gmail.com>
2025-06-07 15:54:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
     function toggleMenu() {
+      const toggleBtn = document.querySelector('.menu-toggle');
       const nav = document.querySelector('.menu');
       nav.classList.toggle('show');
       nav.classList.toggle('hidden');

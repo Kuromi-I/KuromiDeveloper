aa011da3f03f69fe95ebe941e4ea070e73a04ae7
Isa Vieira <isadoravieira2001@gmail.com>
2025-06-08 15:14:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,30 +5,51 @@
 
   toggle.addEventListener("click", () => {
     if (!isVisible) {
-      // Mostra com animação
+
       menu.classList.remove("fade-out");
       menu.style.display = "flex";
-      void menu.offsetWidth; // força reflow para reiniciar a animação
+      void menu.offsetWidth; 
       menu.classList.add("fade-in");
       isVisible = true;
     } else {
-      // Esconde com fade-out
+
       menu.classList.remove("fade-in");
       menu.classList.add("fade-out");
 
-      // Espera a animação terminar e esconde com display: none
+
       setTimeout(() => {
         menu.style.display = "none";
         isVisible = false;
-      }, 300); // mesmo tempo da animação
+      }, 300); 
     } 
 });
+// Opacity
 let timeout;
 window.addEventListener('scroll', () => {
-  document.body.style.opacity = '0.9'; // enquanto rola
+  document.body.style.opacity = '0.9'; 
 
   clearTimeout(timeout);
   timeout = setTimeout(() => {
-    document.body.style.opacity = '1'; // quando parar
+    document.body.style.opacity = '1'; 
   }, 150);
+});
+
+//Dark  Mode
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+const botao = document.getElementById('toggle-darkmode');
+const corpo = document.body;
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  corpo.classList.add('dark-mode');
+  botao.textContent = 'Dark Mode';
+}
+botao.addEventListener('click', () => {
+  corpo.classList.toggle('dark-mode');
+  if (corpo.classList.contains('dark-mode')) {
+    botao.textContent = 'Light Mode';
+  } else {
+    botao.textContent = 'Dark Mode';
+  }
 });

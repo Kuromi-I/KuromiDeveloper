b012532466e22762ad02aa2bede3141f959a8e3e
Isa Vieira <isadoravieira2001@gmail.com>
2025-06-24 18:57:27 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -28,3 +28,15 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.error('Erro ao registrar SW:', err));
   });
 }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).catch(() => {
+        
+        if (event.request.mode === 'navigate') {
+          return caches.match('/offline.html');
+        }
+      });
+    })
+  );
+});
